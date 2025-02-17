0893f33a5e5bc5947e594c8c3b4ffae6e394bd3c
Andrej_Dudelev <dudzik18@gmail.com>
2025-02-17 22:03:22 +0200

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -1,47 +1,41 @@
 
 from datetime import date
+import dataclasses
 import requests
 from bs4 import BeautifulSoup
 from config import URL, headers, CURRENCY_MAP
+import logging
 
 def scrape_rates_for_date(date_obj):
-    """Parsiunčia duomenis pagal datą ir grąžina sąrašą su valiutų kursais."""
+    """Parsiuncia duomenis pagal data ir grazina sarasa su valiutu kursais."""
     date_str = date_obj.strftime("%Y-%m-%d")
     url = URL.format(date_str)
-    print(f"Scraping data for date: {date_str}")
+    logging.info(f"Scraping data for date: {date_str}")
 
     response = requests.get(url, headers=headers)
     if response.status_code != 200:
-        print(f"Error: Nepavyko gauti duomenų už {date_str}")
+        logging.info(f"Error: Nepavyko gauti duomenu uz {date_str}")
         return []
     
     soup = BeautifulSoup(response.text, "html.parser")
     table = soup.find("table", {"class": "ratesTable"})
     if not table:
-        print(f"Valiutų kursų lentelės nerasta {date_str}")
+        logging.info(f"Valiutu kursu lenteles nerasta {date_str}")
         return []
 
     rows = table.find_all("tr")[1:]
-    date = []
+    Data = []
     for row in rows:
         cols = row.find_all("td")
         if len(cols) >= 2:
-            # paima pirmą lentelės stulpelį (valiutos pavadinimą)
             currency_name = cols[0].get_text(strip=True)
-            # paima antrą lentelės stulpelį (valiutos kursą kaip tekstą)
             rate_text = cols[1].get_text(strip=True)
             try:
-                # bando konvertuoti kursą iš teksto į skaičių (float).
                 rate = float(rate_text)
             except ValueError:
                 continue
-            # Tikriname, ar eiluteje esantis valiutos pavadinimas atitinka viena is musu norimu
             for key in CURRENCY_MAP :
                 if key in currency_name:
                     currencies_code = CURRENCY_MAP [key]
-                    date.append((currencies_code, rate, date_str))
+                Data.append((currencies_code, rate, date_str))
     return date 
-
-# xrates_response = requests.get("https://www.x-rates.com/historical/?from=EUR&date={}", headers=headers)
-# print(xrates_response.status_code)
-# xrates_html = xrates_response.text
