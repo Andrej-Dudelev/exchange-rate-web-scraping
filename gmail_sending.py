0b30fa3df58e1e8c4b76ccc9251b94fcea93a4d1
Andrej_Dudelev <dudzik18@gmail.com>
2025-02-17 22:02:47 +0200

    Update gmail_sending.py

--- gmail_sending.py
+++ gmail_sending.py
@@ -1,32 +1,60 @@
+import base64
+import os.path
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from email.message import EmailMessage
-import os 
-import base64
 
-# Kelias iki OAuth 2.0 credentials.json failo
-CREDENTIALS_FILE = "credentials.json"
 
-SCOPES = ["https://www.googleapis.com/auth/gmail.sending"]
+class GmailClient:
+    SCOPES = [
+        "https://www.googleapis.com/auth/gmail.readonly",
+        "https://www.googleapis.com/auth/gmail.send",
+    ]
+    TOKEN_PATH = "./creds/token.json"
+    CREDENTIALS_PATH = "./creds/credentials.json"
 
-def gmail_sending(subject, body, recipient_email):
-    """
-    Siunčia el. laišką naudojant Gmail API.
-    """
-    creds = service_account.Credentials.from_service_account_file(
-        CREDENTIALS_FILE, scopes=SCOPES
-    )
+    def __init__(self):
+        self.creds = None
+        self.service = None
+        self.authenticate()
 
-    service = build("gmail", "v1", credentials=creds)
-    message = MIMEText(body)
-    message["to"] = recipient_email
-    message["subject"] = subject
-    raw_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
-    # koduojame ir siunciame el.laiska
-    message = {"raw": raw_message}
-    service.users().messages().send(userId="me", body=message).execute()
-    print("El. laiskas issiustas:", subject)
-  
+    def authenticate(self):
+        if os.path.exists(self.TOKEN_PATH):
+            self.creds = Credentials.from_authorized_user_file(
+                filename=self.TOKEN_PATH, scopes=self.SCOPES
+            )
+        if not self.creds or not self.creds.valid:
+            if self.creds and self.creds.expired and self.creds.refresh_token:
+                self.creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    client_secrets_file=self.CREDENTIALS_PATH, scopes=self.SCOPES
+                )
+                self.creds = flow.run_local_server(port=0)
+            with open(self.TOKEN_PATH, "w") as token:
+                token.write(self.creds.to_json())
+
+        self.service = build(serviceName="gmail", version="v1", credentials=self.creds)
+
+    def send_email(self, to_email, subject, content):
+        try:
+            message = EmailMessage()
+            message.set_content(content)
+            message["To"] = to_email
+            message["Subject"] = subject
+
+            encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
+            create_message = {"raw": encoded_message}
+
+            send_message = (
+                self.service.users()
+                .messages()
+                .send(userId="me", body=create_message)
+                .execute()
+            )
+            return f'Message Id: {send_message["id"]}'
+        except HttpError as error:
+            return f"An error occurred: {error}"
