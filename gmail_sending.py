ff2fb4dd7efaca7364946f6a40ada566eb8811a9
Andrej_Dudelev <dudzik18@gmail.com>
2025-02-17 17:52:37 +0200

    Update gmail_sending.py

--- gmail_sending.py
+++ gmail_sending.py
@@ -7,4 +7,26 @@ from email.message import EmailMessage
 import os 
 import base64
 
-recipient_email = "gavejas@gmail.com"
+# Kelias iki OAuth 2.0 credentials.json failo
+CREDENTIALS_FILE = "credentials.json"
+
+SCOPES = ["https://www.googleapis.com/auth/gmail.sending"]
+
+def gmail_sending(subject, body, recipient_email):
+    """
+    Siunčia el. laišką naudojant Gmail API.
+    """
+    creds = service_account.Credentials.from_service_account_file(
+        CREDENTIALS_FILE, scopes=SCOPES
+    )
+
+    service = build("gmail", "v1", credentials=creds)
+    message = MIMEText(body)
+    message["to"] = recipient_email
+    message["subject"] = subject
+    raw_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
+    # koduojame ir siunciame el.laiska
+    message = {"raw": raw_message}
+    service.users().messages().send(userId="me", body=message).execute()
+    print("El. laiskas issiustas:", subject)
+  
